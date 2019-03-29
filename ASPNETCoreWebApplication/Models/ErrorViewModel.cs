ef1e25751a6b2d6ac1d27925f8525328734b12e4
Christopher  Holley <holleyc7@me.com>
2019-03-29 13:59:47 -0400

    Adding in the Basic ASP.NET Core Web Application

---
+++ ASPNETCoreWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETCoreWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
